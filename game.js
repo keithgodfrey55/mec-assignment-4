f9211e74898933ae711261489914f3af2ca986f0
Michael Casey <caseymichaelt1893@gmail.com>
2021-01-08 16:59:12 -0500

    Fixed Issue

--- game.js
+++ game.js
@@ -1,20 +1,23 @@
 let board = [
-    [' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' '],
+    [' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' '],
 ];
-function makeBoard(board){
-      for(let i = 0; i < 7; i++){
-        for(let x = 0; x < 6; x++){
+
+function makeBoard(board) {
+    for (let i = 0; i < 7; i++) {
+        for (let x = 0; x < 6; x++) {
             let a = board[i][x];
             console.log(a);
         }
 
-      }
+    }
 }
 
-  
+function playerTurn() {
+    document.getElementById("slot0-0").style.backgroundColor = "red";
+}
