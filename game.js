34e21a43248b977c76f1cb4e4a623afcf38f67a2
Keith Godfrey <keithgodfrey@Keiths-MacBook-Pro.local>
2021-01-08 18:55:06 -0500

    slot function

--- game.js
+++ game.js
@@ -7,17 +7,42 @@ let board = [
     [' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' '],
 ];
-
+let playerMove = true;
 function makeBoard(board) {
     for (let i = 0; i < 7; i++) {
         for (let x = 0; x < 6; x++) {
-            let a = board[i][x];
-            console.log(a);
+            document.getElementById(`slot${i}-${x}`).style.backgroundColor = "white";
+            console.log(document.getElementById(`slot${i}-${x}`));
         }
 
     }
 }
+function checkMove(y){
+    console.log('inside function');
+    for(let x = 6; x >= 0; x--){
+        console.log('inside loop');
+        if(document.getElementById(`slot${x}-${y}`).style.backgroundColor == "white"){
+            console.log('inside if');
+            if(playerMove === true){
+                document.getElementById(`slot${x}-${y}`).style.backgroundColor = "red";
+                console.log(x);
+                playerMove = false;
+                break;
+            }
+            else{
+                document.getElementById(`slot${x}-${y}`).style.backgroundColor = "blue";
+                console.log(x);
+                playerMove = true;
+            break;
+            }
+        
+        }
+    }
 
-function playerTurn() {
-    document.getElementById("slot0-0").style.backgroundColor = "red";
 }
+
+makeBoard(board);
+/*if if its the player turn & they click on a column then 
+check the most bottom slot 
+for availibilty. 
+color change on the most bottom availible slot for the column  */
