84af1e313ed9e3f7907a3bb400404164dea2374d
Michael Casey <caseymichaelt1893@gmail.com>
2021-01-10 00:32:34 -0500

    Further changes

--- game.js
+++ game.js
@@ -30,6 +30,7 @@ function checkHorizontal() {
                     console.log("Player Wins")
                     wins += 1;
                     alert("Player Wins")
+                    document.getElementById("win").innerHTML = "Wins: " + wins;
                     return true;
                 }
 
@@ -38,6 +39,7 @@ function checkHorizontal() {
                     console.log("AI Wins")
                     losses += 1;
                     alert("AI Wins")
+                    document.getElementById("lose").innerHTML = "Losses: " + losses;
                     return true;
                 }
             }
@@ -53,12 +55,14 @@ function checkVertical() {
                     console.log("Player Wins")
                     wins += 1;
                     alert("Player Wins")
+                    document.getElementById("win").innerHTML = "Wins: " + wins;
                     return true;
                 }
             } else if ((board[i][j] == "blue") && (board[i - 1][j] == "blue") && (board[i - 2][j] == "blue") && (board[i - 3][j] == "blue")) {
                 if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "blue") {
                     losses += 1;
                     alert("AI Wins")
+                    document.getElementById("lose").innerHTML = "Losses: " + losses;
                     return true;
                 }
             }
@@ -71,14 +75,18 @@ function checkVertical() {
 function checkDiagonalRight() {
     for (let i = 6; i > 0; i--) {
         for (let j = 0; j < 3; j++) {
-            if ((board[i][j] == board[i - 1][j + 1]) && (board[i][j] == board[i - 2][j + 2]) && (board[i][j] == board[i - 3][j + 3])) {
+            if ((board[i][j] == "red") && (board[i - 1][j + 1] == "red") && (board[i - 2][j + 2] == "red") && (board[i - 3][j + 3] == "red")) {
                 if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "red") {
-                    alert("Player Wins")
                     wins += 1;
+                    alert("Player Wins")
+                    document.getElementById("win").innerHTML = "Wins: " + wins;
                     return true;
-                } else if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "blue") {
-                    alert("AI Wins")
+                }
+            } else if ((board[i][j] == "blue") && (board[i - 1][j + 1] == "blue") && (board[i - 2][j + 2] == "blue") && (board[i - 3][j + 3] == "blue")) {
+                if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "blue") {
                     losses += 1;
+                    alert("AI Wins")
+                    document.getElementById("lose").innerHTML = "Losses: " + losses;
                     return true;
                 }
             }
@@ -142,7 +150,7 @@ function move(select) {
     botMove();
     checkHorizontal();
     checkVertical();
-    // checkDiagonalRight();
+    checkDiagonalRight();
 
 
 }
