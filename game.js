3c59fd16ca9d49cb7059b8f99c6d35b08237938e
Lulu <luismora.11131@gmail.com>
2021-01-10 21:27:59 -0500

    final version

--- game.js
+++ game.js
@@ -10,37 +10,47 @@ let board = [
 let playerMove = true;
 let wins = 0;
 let losses = 0;
-let ties = 0;
+let ties = 1;
+let whites = 42;
 
 function makeBoard(board) {
     for (let i = 0; i < 7; i++) {
         for (let x = 0; x < 6; x++) {
             document.getElementById(`slot${i}-${x}`).style.backgroundColor = "white";
-            board[i][x] = "white"
+            board[i][x] = "white";
         }
 
     }
 }
 
+function tie(){
+    if(whites==0){
+        document.getElementById("tie").innerHTML = "Ties: " + ties;
+        ties+=1;
+        alert("Its a Tie!");
+    }
+}
 function checkHorizontal() {
     for (let i = 6; i > 0; i--) {
         for (let j = 0; j < 3; j++) {
-            if ((board[i][j] == "red") && (board[i][j + 1] == "red") && (board[i][j + 2] == "red") && (board[i][j + 3] == "red")) {
+            if ((board[i][j] === "red") && (board[i][j + 1] === "red") && (board[i][j + 2] === "red") && (board[i][j + 3] === "red")) {
                 if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "red") {
                     console.log("Player Wins")
                     wins += 1;
                     alert("Player Wins")
                     document.getElementById("win").innerHTML = "Wins: " + wins;
-                    return true;
+                    // return true;
+                    console.log('this was a horizontal win')
                 }
 
-            } else if ((board[i][j] == "blue") && (board[i][j + 1] == "blue") && (board[i][j + 2] == "blue") && (board[i][j + 3] == "blue")) {
+            } else if ((board[i][j] === "blue") && (board[i][j + 1] === "blue") && (board[i][j + 2] === "blue") && (board[i][j + 3] === "blue")) {
                 if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "blue") {
                     console.log("AI Wins")
                     losses += 1;
                     alert("AI Wins")
                     document.getElementById("lose").innerHTML = "Losses: " + losses;
-                    return true;
+                    // return true;
+                    console.log('this was a horizontal win')
                 }
             }
         }
@@ -48,23 +58,25 @@ function checkHorizontal() {
 }
 
 function checkVertical() {
-    for (let i = 6; i > 0; i--) {
-        for (let j = 0; j < 3; j++) {
-            if ((board[i][j] == "red") && (board[i - 1][j] == "red") && (board[i - 2][j] == "red") && (board[i - 3][j] == "red")) {
-                if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "red") {
+    for (let i = 6; i > 3; i--) {
+        for (let j = 0; j < 6; j++) {
+            if ((board[i][j] === "red") && (board[i - 1][j] === "red") && (board[i - 2][j] === "red") && (board[i - 3][j] === "red")) {
+                // if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "red") {
                     console.log("Player Wins")
                     wins += 1;
                     alert("Player Wins")
                     document.getElementById("win").innerHTML = "Wins: " + wins;
-                    return true;
-                }
-            } else if ((board[i][j] == "blue") && (board[i - 1][j] == "blue") && (board[i - 2][j] == "blue") && (board[i - 3][j] == "blue")) {
-                if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "blue") {
+                    console.log('this was a vertical win')
+                    // return true;
+                // }
+            } else if ((board[i][j] === "blue") && (board[i - 1][j] === "blue") && (board[i - 2][j] === "blue") && (board[i - 3][j] === "blue")) {
+                // if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "blue") {
                     losses += 1;
                     alert("AI Wins")
                     document.getElementById("lose").innerHTML = "Losses: " + losses;
-                    return true;
-                }
+                    console.log('this was a vertical win')
+                    // return true;
+                // }
             }
         }
     }
@@ -73,21 +85,23 @@ function checkVertical() {
 
 
 function checkDiagonalRight() {
-    for (let i = 6; i > 0; i--) {
+    for (let i = 6; i > 3; i--) {
         for (let j = 0; j < 3; j++) {
             if ((board[i][j] == "red") && (board[i - 1][j + 1] == "red") && (board[i - 2][j + 2] == "red") && (board[i - 3][j + 3] == "red")) {
                 if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "red") {
                     wins += 1;
                     alert("Player Wins")
                     document.getElementById("win").innerHTML = "Wins: " + wins;
-                    return true;
+                    // return true;
+                    console.log('this was a diagonal right win')
                 }
             } else if ((board[i][j] == "blue") && (board[i - 1][j + 1] == "blue") && (board[i - 2][j + 2] == "blue") && (board[i - 3][j + 3] == "blue")) {
                 if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "blue") {
                     losses += 1;
                     alert("AI Wins")
                     document.getElementById("lose").innerHTML = "Losses: " + losses;
-                    return true;
+                    // return true;
+                    console.log('this was a diagonal right win')
                 }
             }
         }
@@ -95,18 +109,23 @@ function checkDiagonalRight() {
 }
 
 function checkDiagonalLeft() {
-    for (let i = 6; i > 0; i--) {
-        for (let j = 0; j < 3; j++) {
-            if ((board[i][j] == board[i - 1][j - 1]) && (board[i][j] == board[i - 2][j - 2]) && (board[i][j] == board[i - 3][j - 3])) {
+    for (let i = 6; i > 3; i--) {
+        for (let j = 5; j >= 3; j--) {
+            if ((board[i][j] == "red") && (board[i - 1][j - 1] == "red") && (board[i - 2][j - 2] == "red") && (board[i - 3][j - 3] == "red")) {
                 if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "red") {
                     wins += 1;
-                    console.log("Player Wins")
-                    return true;
-                } else if (document.getElementById(`slot${i}-${j}`).style.backgroundColor = "blue") {
-                    console.log("AI Wins")
-                    losses += 1;
-                    console.log("AI Wins")
-                    return true;
+                    alert("Player Wins")
+                    document.getElementById("win").innerHTML = "Wins: " + wins;
+                    // return true;
+                    console.log('this was a diagonal left win')
+                }
+                } else if ((board[i][j] == "blue") && (board[i - 1][j - 1] == "blue") && (board[i - 2][j - 2] == "blue") && (board[i - 3][j - 3] == "blue")) {
+                    if (document.getElementById(`slot${i}-${j}`).style.backgroundColor === "blue") {
+                        losses += 1;
+                        alert("AI Wins")
+                        document.getElementById("lose").innerHTML = "Losses: " + losses;
+                        console.log('this was a diagonal left win')
+                        // return true;
                 }
             }
         }
@@ -120,12 +139,14 @@ function checkMove(y) {
                 document.getElementById(`slot${x}-${y}`).style.backgroundColor = "red";
                 // checkVertical();
                 playerMove = false;
-                board[x][y] = "red"
+                board[x][y] = "red";
+                whites-=1;
                 break;
             } else {
                 document.getElementById(`slot${x}-${y}`).style.backgroundColor = "blue";
                 playerMove = true;
-                board[x][y] = "blue"
+                board[x][y] = "blue";
+                whites-=1;
                 break;
             }
 
@@ -142,16 +163,29 @@ function reset() {
 
 function botMove() {
     let bot_move = Math.floor(Math.random() * 6);
-    checkMove(bot_move);
+    let condition = true;
+    while(condition){
+            if(board[0][bot_move]=="white"){
+                checkMove(bot_move);
+                condition=false; 
+                }
+            else{
+                bot_move = Math.floor(Math.random() * 6);
+                 
+        }
+    }
+    
 }
 
 function move(select) {
     checkMove(select);
+    tie();
     botMove();
     checkHorizontal();
     checkVertical();
     checkDiagonalRight();
-
+    checkDiagonalLeft();
+    
 
 }
 
